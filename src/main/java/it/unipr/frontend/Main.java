391dc5af2cbddf181835eff00aceef6290554def
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-03-31 14:16:59 +0200

    Added Interval analysis in Main

--- src/main/java/it/unipr/frontend/Main.java
+++ src/main/java/it/unipr/frontend/Main.java
@@ -1,11 +1,19 @@
 package it.unipr.frontend;
 
-import it.unipr.frontend.reg.RegLiSAFrontend;
-import it.unive.lisa.DefaultConfiguration;
-import it.unive.lisa.LiSA;
-import it.unive.lisa.conf.LiSAConfiguration;
-import it.unive.lisa.program.Program;
 import java.io.IOException;
+
+import it.unipr.frontend.reg.RegLiSAFrontend;
+import it.unive.lisa.LiSA;
+import it.unive.lisa.analysis.SimpleAbstractState;
+import it.unive.lisa.analysis.heap.MonolithicHeap;
+import it.unive.lisa.analysis.nonrelational.value.TypeEnvironment;
+import it.unive.lisa.analysis.nonrelational.value.ValueEnvironment;
+import it.unive.lisa.analysis.numeric.Interval;
+import it.unive.lisa.analysis.types.InferredTypes;
+import it.unive.lisa.conf.LiSAConfiguration;
+import it.unive.lisa.interprocedural.ModularWorstCaseAnalysis;
+import it.unive.lisa.interprocedural.callgraph.RTACallGraph;
+import it.unive.lisa.program.Program;
 
 public class Main {
 
@@ -14,12 +22,18 @@ public class Main {
 		System.out.println("File: " + file);
 
 		Program program = RegLiSAFrontend.processFile(args[0]);
-		LiSAConfiguration conf = new DefaultConfiguration();
-
+		LiSAConfiguration conf = new LiSAConfiguration();
 		conf.workdir = "reglisa-outputs";
-		conf.analysisGraphs = LiSAConfiguration.GraphType.HTML;
+		conf.analysisGraphs = LiSAConfiguration.GraphType.DOT;
+		
+		conf.abstractState = new SimpleAbstractState<>(new MonolithicHeap(),
+				new ValueEnvironment<>(new Interval()),
+				new TypeEnvironment<>(new InferredTypes()));
+		
+		conf.interproceduralAnalysis = new ModularWorstCaseAnalysis<>();
+		conf.callGraph = new RTACallGraph();
 		conf.serializeResults = true;
-		conf.serializeInputs = true;
+		conf.optimize = false;
 		conf.jsonOutput = true;
 
 		LiSA lisa = new LiSA(conf);
